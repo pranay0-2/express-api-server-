03657a900241ddfb3bb01d251fe9fa0240205f2d
Pranay <pranayrajput3611675@gmail.com>
2024-10-04 14:32:12 +0530

    modified: index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const port = 3000
+
+const githubData = {"Login" : "pranay"};
 
 app.get('/', (req , res) =>{
     res.send('Hello World')
@@ -8,6 +12,10 @@ app.get('/', (req , res) =>{
 
 app.get('/login', (req , res) =>{
     res.send('Please Login!')
+})
+
+app.get('/github' , (req,res) =>{
+    res.send(githubData)
 })
 
 app.listen(port, () =>{
